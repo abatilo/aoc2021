3a406a528c7d300cfeac100e1ab780be5937d39d
Aaron <AaronBatilo@gmail.com>
2021-12-05 12:17:55 -0700

    Day 05 - 2 Star

--- day05/main.go
+++ day05/main.go
@@ -8,244 +8,131 @@ import (
 	"strings"
 )
 
-// Vector2D represents a 2D vector
-type Vector2D struct {
-	X int
-	Y int
-}
-
-// Vector2DPairs represents two Vector2D objects
-type Vector2DPairs struct {
-	A Vector2D
-	B Vector2D
-}
-
-// VectorPath takes a VectorPairs and returns a slice of Vector2D objects for each point bewteen them and prints the pairs
-func VectorPath(pairs Vector2DPairs) []Vector2D {
-	// Create a slice of Vector2D objects
-	path := make([]Vector2D, 0)
-
-	// Add the first pair
-	path = append(path, pairs.A)
-
-	// Add the rest of the pairs
-	for x := pairs.A.X; x != pairs.B.X; {
-		if x < pairs.B.X {
-			x++
-		} else {
-			x--
-		}
-		path = append(path, Vector2D{x, pairs.A.Y})
-	}
-	for y := pairs.A.Y; y != pairs.B.Y; {
-		if y < pairs.B.Y {
-			y++
-		} else {
-			y--
-		}
-		path = append(path, Vector2D{pairs.B.X, y})
-	}
-
-	return path
-}
-
-// VectorFrequencies takes a slice of Vector2D objects and returns a map of Vector2D objects and their frequency
-func VectorFrequencies(path []Vector2D) map[Vector2D]int {
-	frequencies := make(map[Vector2D]int)
-	for _, vector := range path {
-		frequencies[vector]++
-	}
-	return frequencies
-}
-
-// openFile opens a file and returns a file pointer and an error
-func openFile(fileName string) (*os.File, error) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
-}
-
-// parseInputLine takes a string of format "X1,Y1 -> X2,Y2" and returns a Vector2DPairs and converts each coordinate
-// to an int
-func parseInputLine(line string) Vector2DPairs {
-	// Split line into two strings
-	splitLine := strings.Split(line, " -> ")
-
-	// Split first string into two coordinates
-	coordinates1 := strings.Split(splitLine[0], ",")
-
-	// Split second string into two coordinates
-	coordinates2 := strings.Split(splitLine[1], ",")
-
-	// Convert coordinates to ints
-	x1, _ := strconv.Atoi(coordinates1[0])
-	y1, _ := strconv.Atoi(coordinates1[1])
-	x2, _ := strconv.Atoi(coordinates2[0])
-	y2, _ := strconv.Atoi(coordinates2[1])
-
-	// Create Vector2DPairs
-	pairs := Vector2DPairs{Vector2D{x1, y1}, Vector2D{x2, y2}}
-
-	return pairs
-}
-
-// println is a convenience function for printing to stdout
-func println(a ...interface{}) (int, error) {
-	return fmt.Println(a...)
-}
-
-// PrettyPrintGrid takes a slice of Vector2D objects and prints them in a 9x9 grid
-func PrettyPrintGrid(path []Vector2D) {
-	// Find the min and max X and Y values
-	var minX, maxX, minY, maxY int
-	for _, vector := range path {
-		if vector.X < minX {
-			minX = vector.X
-		}
-		if vector.X > maxX {
-			maxX = vector.X
-		}
-		if vector.Y < minY {
-			minY = vector.Y
-		}
-		if vector.Y > maxY {
-			maxY = vector.Y
+// max returns the max of all ints from varargs
+func findMax(integers ...int) int {
+	var result int
+	for _, v := range integers {
+		if v > result {
+			result = v
 		}
 	}
-
-	// Print the grid
-	for y := minY; y <= maxY; y++ {
-		for x := minX; x <= maxX; x++ {
-			var found bool
-			for _, vector := range path {
-				if vector.X == x && vector.Y == y {
-					found = true
-					break
-				}
-			}
-			if found {
-				fmt.Print("#")
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
-	}
-}
-
-// PrettyPrintFrequencies takes a map of Vector2D objects and their frequencies and prints the frequency in a grid
-func PrettyPrintFrequencies(frequencies map[Vector2D]int) {
-	// Find the min and max X and Y values
-	var minX, maxX, minY, maxY int
-	for vector, _ := range frequencies {
-		if vector.X < minX {
-			minX = vector.X
-		}
-		if vector.X > maxX {
-			maxX = vector.X
-		}
-		if vector.Y < minY {
-			minY = vector.Y
-		}
-		if vector.Y > maxY {
-			maxY = vector.Y
-		}
-	}
-
-	// Print the grid
-	for y := minY; y <= maxY; y++ {
-		for x := minX; x <= maxX; x++ {
-			var found bool
-			for vector, _ := range frequencies {
-				if vector.X == x && vector.Y == y {
-					found = true
-					break
-				}
-			}
-			if found {
-				fmt.Print(frequencies[Vector2D{x, y}])
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
-	}
+	return result
 }
 
 func main() {
-	// Open file input.txt and ignore errors
-	file, _ := openFile("input.txt")
+	file, _ := os.Open("input.txt")
 	defer file.Close()
 
 	// Read file line by line
 	scanner := bufio.NewScanner(file)
 
-	// Create a slice of Vector2DPairs by reading each line
-	var pairs []Vector2DPairs
+	lines := []string{}
 	for scanner.Scan() {
-		pairs = append(pairs, parseInputLine(scanner.Text()))
+		lines = append(lines, scanner.Text())
 	}
 
-	// Keep pairs that are either horizontal or vertical
-	var horizontalPairs, verticalPairs []Vector2DPairs
-	for _, pair := range pairs {
-		if pair.A.X == pair.B.X {
-			verticalPairs = append(verticalPairs, pair)
-		} else if pair.A.Y == pair.B.Y {
-			horizontalPairs = append(horizontalPairs, pair)
+	var max int
+
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		a, b := fields[0], fields[len(fields)-1]
+
+		x1, _ := strconv.Atoi(strings.Split(a, ",")[0])
+		y1, _ := strconv.Atoi(strings.Split(a, ",")[1])
+
+		x2, _ := strconv.Atoi(strings.Split(b, ",")[0])
+		y2, _ := strconv.Atoi(strings.Split(b, ",")[1])
+
+		max = findMax(max, x1, y1, x2, y2)
+	}
+
+	grid := [][]int{}
+	for i := 0; i <= max; i++ {
+		grid = append(grid, make([]int, max+1))
+	}
+
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		a, b := fields[0], fields[len(fields)-1]
+
+		x1, _ := strconv.Atoi(strings.Split(a, ",")[0])
+		y1, _ := strconv.Atoi(strings.Split(a, ",")[1])
+
+		x2, _ := strconv.Atoi(strings.Split(b, ",")[0])
+		y2, _ := strconv.Atoi(strings.Split(b, ",")[1])
+
+		fmt.Printf("Moving between (%d, %d) -> (%d, %d)\n", x1, y1, x2, y2)
+		if x1 == x2 {
+			fmt.Println("Moving vertically")
+			if y1 < y2 {
+				fmt.Println("Moving down")
+				for i := y1; i <= y2; i++ {
+					grid[i][x1]++
+				}
+			} else {
+				fmt.Println("Moving up")
+				for i := y2; i <= y1; i++ {
+					grid[i][x1]++
+				}
+			}
+		} else if y1 == y2 {
+			fmt.Println("Moving horizontally")
+			if x1 < x2 {
+				fmt.Println("Moving right")
+				for i := x1; i <= x2; i++ {
+					grid[y1][i]++
+				}
+			} else {
+				fmt.Println("Moving left")
+				for i := x2; i <= x1; i++ {
+					grid[y1][i]++
+				}
+			}
+		} else {
+			fmt.Println("Moving diagonally")
+			var xDir, yDir int
+
+			if x1 <= x2 {
+				xDir = 1
+			} else {
+				xDir = -1
+			}
+
+			if y1 <= y2 {
+				yDir = 1
+			} else {
+				yDir = -1
+			}
+
+			i, j := x1, y1
+
+			for i != x2 || j != y2 {
+				grid[j][i] += 1
+				i += xDir
+				j += yDir
+			}
+			grid[j][i] += 1
 		}
+		fmt.Println("----")
 	}
 
-	// combine horizontalpairs and verticalparis into a single slice of Vector2DPairs
-	var combinedPairs []Vector2DPairs
-	for _, pair := range horizontalPairs {
-		combinedPairs = append(combinedPairs, pair)
-	}
-	for _, pair := range verticalPairs {
-		combinedPairs = append(combinedPairs, pair)
-	}
-
-	// print the combined pairs
-	println("Combined pairs:")
-	for _, pair := range combinedPairs {
-		println(pair.A, " -> ", pair.B)
-	}
-
-	// print vector path of first horizontal pair
-	println("First horizontal pair:")
-	println(VectorPath(combinedPairs[0]))
-
-	// Create a slice of Vector2D objects by combining all the horizontal and vertical paths
-	var path []Vector2D
-	for _, pair := range horizontalPairs {
-		path = append(path, VectorPath(pair)...)
-	}
-	for _, pair := range verticalPairs {
-		path = append(path, VectorPath(pair)...)
-	}
-
-	// print all paths
-	println("All paths:")
-	for _, vector := range path {
-		println(vector)
-	}
-
-	// print line divider
-	println()
-
-	// calculate vector frequencies
-	frequencies := VectorFrequencies(path)
-
-	// count the number of times 2 or more vectors appear
+	// Count numbers above 1 in grid
 	var count int
-	for _, frequency := range frequencies {
-		if frequency >= 2 {
-			count++
+	for _, row := range grid {
+		for _, v := range row {
+			if v > 1 {
+				count++
+			}
 		}
 	}
 
-	// print the number of times 2 or more vectors appear
-	println("Number of times 2 or more vectors appear:", count)
+	// pretty print grid
+	for i := 0; i <= max; i++ {
+		for j := 0; j <= max; j++ {
+			fmt.Printf("%d ", grid[i][j])
+		}
+		fmt.Println()
+	}
+
+	fmt.Println(count)
 }
